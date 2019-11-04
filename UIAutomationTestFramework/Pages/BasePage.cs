a6684d097f365d634d62c64cb781afd395f26cdc
angelrbee <56300263+angelrbee@users.noreply.github.com>
2019-11-04 21:36:37 +1100

    Add project files.

---
+++ UIAutomationTestFramework/Pages/BasePage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium.Support.PageObjects;
+
+namespace UIAutomationTestFramework.Pages
+{
+    public class BasePage
+    {
+        private static T GetPage<T>() where T : new()
+        {
+            var page = new T();
+            PageFactory.InitElements(Browser.Driver, page);
+            return page;
+        }
+
+        public static HomePage HomePage
+        {
+            get { return GetPage<HomePage>(); }
+        }
+    }
+
+}
